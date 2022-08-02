e52f7fbe098ba935a34835dbd0d72fa33f4377e4
Shin-sangwon <dnjsml10@gmail.com>
2022-08-02 15:20:12 +0900

    @repository 구현 및 코드 리팩토링

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -2,6 +2,7 @@ package com.ll.exam;
 
 import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
+import com.ll.exam.annotation.Repository;
 import com.ll.exam.annotation.Service;
 import org.reflections.Reflections;
 
@@ -19,12 +20,14 @@ public class Container {
     }
 
     private static void scanComponents() {
+        scanRepositories();
         scanServices();
         scanControllers();
 
         //조립
         resolveDependenciesAllComponents();
     }
+
 
     private static void resolveDependenciesAllComponents() {
         for ( var cls : objects.keySet()) {
@@ -55,15 +58,22 @@ public class Container {
     }
 
     private static void scanControllers() {
-        Reflections ref = new Reflections("com.ll.exam");
+        Reflections ref = new Reflections(App.BASE_PACKAGE_PATH);
         for (Class<?> cls : ref.getTypesAnnotatedWith(Controller.class)){
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
     }
 
     private static void scanServices() {
-        Reflections ref = new Reflections("com.ll.exam");
+        Reflections ref = new Reflections(App.BASE_PACKAGE_PATH);
         for (Class<?> cls : ref.getTypesAnnotatedWith(Service.class)){
+            objects.put(cls, Ut.cls.newObj(cls, null));
+        }
+    }
+
+    private static void scanRepositories() {
+        Reflections ref = new Reflections(App.BASE_PACKAGE_PATH);
+        for (Class<?> cls : ref.getTypesAnnotatedWith(Repository.class)){
             objects.put(cls, Ut.cls.newObj(cls, null));
         }
     }
