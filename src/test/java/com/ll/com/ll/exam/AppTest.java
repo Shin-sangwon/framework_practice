4dc8148e6f8107a686ea2ad6cb2ca0edfc5f1437
Shin-sangwon <dnjsml10@gmail.com>
2022-08-02 09:36:02 +0900

    project init

---
+++ src/test/java/com/ll/com/ll/exam/AppTest.java
@@ -0,0 +1,15 @@
+package com.ll.com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AppTest {
+
+    @Test
+    public void junit_assertThat(){
+        int rs = 10 + 20;
+
+        assertThat(rs).isEqualTo(30);
+    }
+}
