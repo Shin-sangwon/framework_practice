941ac7ae5191f10c94333cc4108a776267db2cb2
Shin-sangwon <dnjsml10@gmail.com>
2022-08-02 14:09:56 +0900

    @Service 도입

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -2,10 +2,11 @@ package com.ll.exam.article.controller;
 
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.GetMapping;
+import com.ll.exam.article.service.ArticleService;
 
 @Controller // 컨트롤러임을 알려줌
 public class ArticleController {
-
+    private ArticleService articleService;
     @GetMapping("/usr/article/list") // /usr/article/list와 관련된 요청 처리
     public void showList() {
 
